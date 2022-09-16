55cdce61051c7c0fb05a1ae2a00a7fba560c979d
mdrgoncalves <mdr.rgon@gmail.com>
2022-09-15 22:05:17 -0300

    fix: general fixes

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 //DOM Consts
-const body = document.body;
+const htmlElement = document.querySelector("html");
 const headerButton = document.querySelector('.header__menu');
 const menu = document.querySelector('.menu');
 const menuCross = document.querySelector('.menu__cross');
@@ -8,11 +8,11 @@ const menuCross = document.querySelector('.menu__cross');
 /* Adding an event listener to the headerButton. When the headerButton is clicked, the menu appears */
 headerButton.addEventListener('click', function() {
     menu.classList.add('menu--active');
-    body.classList.add('menu--hidden-overflow');
+    htmlElement.classList.add('menu--hidden-overflow');
 });
 
 /* Adding an event listener to the menuCross. When the menuCross is clicked, the menu disappears. */
 menuCross.addEventListener('click', function() {
     menu.classList.remove('menu--active');
-    body.classList.remove('menu--hidden-overflow');
+    htmlElement.classList.remove('menu--hidden-overflow');
 });
